d2af88806dc7e7c153ff0e4e1b31dd629cba4bbd
Masha <masha.solodova1@gmail.com>
2015-09-22 10:37:43 +0300

    OrganizationController created, addOrganization changed

---
+++ src/java/controllers/organizationController.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import java.io.UnsupportedEncodingException;
+import java.sql.SQLException;
+import javax.servlet.http.HttpServletRequest;
+import models.*;
+import sql.Sql;
+
+
+public class organizationController
+{
+    public static void add(HttpServletRequest request) throws ClassNotFoundException, SQLException, UnsupportedEncodingException
+    {
+            String name = request.getParameter("name");
+            name = new String(name.getBytes("iso8859-1"), "utf8");
+            String address = request.getParameter("address");
+            address = new String(address.getBytes("iso8859-1"), "utf8");
+            String website = request.getParameter("website");        
+            DBModel.init();
+            Organization organization = new Organization();
+            organization.setName(Sql.sql(name));
+            organization.setAddress(Sql.sql(address));
+            organization.setWebsite(Sql.sql(website));
+            organization.writeToDB();
+    }
+}
+
