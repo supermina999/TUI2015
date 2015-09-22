ef314a6e2a45ccc5aa5a7f6541ee4cce70eab13d
Stas Minakov <supermina999@gmail.com>
2015-09-22 12:00:16 +0300

    Added Controllers to Station and Stock

--- src/java/controllers/StationController.java
+++ src/java/controllers/StationController.java
@@ -1,8 +1,26 @@
 package controllers;
 
+import models.*;
+import java.sql.*;
+import javax.servlet.http.HttpServletRequest;
+
 public class StationController {
-    public static void add()
+    public static void add(HttpServletRequest request) throws ClassNotFoundException, SQLException
     {
-        
+        String address = request.getParameter("address");
+        String organization_id = request.getParameter("organization");
+        String city_id = request.getParameter("city");
+        DBModel.init();
+        Station station = new Station();
+        Location location = new Location();
+        location.setCityId(Integer.parseInt(city_id));
+        location.setAddress(address);
+        location.setXCoord(1);
+        location.setYCoord(1);
+        location.writeToDB();
+        Location[] all_locations = Location.getAll(null);
+        station.setLocationId(all_locations.length);
+        station.setOrganizationId(Integer.parseInt(organization_id));
+        station.writeToDB();
     }
 }
