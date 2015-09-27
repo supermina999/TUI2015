aaa70077042342481f2250b1e010486a78c0bf79
Roman Skurikhin <roma@MacBook-Air-Roman.local>
2015-09-27 12:28:24 +0300

    search "выберите" fix

--- web/js/search.js
+++ web/js/search.js
@@ -4,15 +4,21 @@ $.each($('.searchInput'),function() {
         var s = $(this).val();
         if ($(this).hasClass("City")) s+=", ";
         s = s.toLowerCase();
-        var $rows = $(document).find("#searchTable").find("tr");
         console.log(s);
-        $.each($rows, function() {
-            var t = $(this).text();
-                t = t.toLowerCase();
-            console.log(t);
-            if (t.contains(s)) $(this).removeClass("hidden");
-            else $(this).addClass("hidden"); 
-        });
+        var $rows = $(document).find("#searchTable").find("tr");
+        if (!s.contains("выберите")) {
+            $.each($rows, function() {
+                var t = $(this).text();
+                    t = t.toLowerCase();
+                console.log(t);
+                if (t.contains(s)) $(this).removeClass("hidden");
+                else $(this).addClass("hidden"); 
+            });
+        } else {
+            $.each($rows, function() {
+                $(this).removeClass("hidden");
+            });
+        }
     };
     $(this).keyup(search);
     $(this).change(search);
