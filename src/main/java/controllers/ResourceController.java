8f6918ffb27b6f9085997f0af6f4500248305980
Maria Solodova <masha.solodova1@gmail.com>
2015-10-02 14:16:10 +0300

    change type of resources added, but does not work

--- src/main/java/controllers/ResourceController.java
+++ src/main/java/controllers/ResourceController.java
@@ -18,6 +18,17 @@ public class ResourceController {
         resource.setWeight(weight);
         resource.writeToDB();
     }
+     public static void change(HttpServletRequest request) throws Exception
+    {
+        String resouce = request.getParameter("resourceId");
+        int resourceId = Integer.parseInt(resource);
+        String number = request.getParameter("weight");
+        int weight = Integer.parseInt(number);
+        
+        Resource resource = Resource.getOne(resourceId);
+        resource.setWeight(weight);
+        resource.saveChanges();
+    }
     public static void transit(HttpServletRequest request) throws Exception
     {
         String resource = request.getParameter("resourceId");
