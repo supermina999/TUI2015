036ce464f432b58a4cdf829da95025f562c1553b
Liza <lizapopova@yandex.ua>
2015-10-06 21:40:15 +0300

    Draggable placemarks added, route between them appears

--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -1,5 +1,5 @@
 ymaps.ready(init);
-var myMap;
+var myMap, start, finish;
 
 function init() { 
     myMap = new ymaps.Map("map", {
@@ -18,8 +18,7 @@ function addPlacemark (address) {
         function (res) {
            if (res.geoObjects.getLength()) {
             var point = res.geoObjects.get(0);
-            $('#lat').val(point.geometry.getCoordinates()[0].toString());
-            $('#lon').val(point.geometry.getCoordinates()[1].toString());
+            addToTable(address, point.geometry.getCoordinates()[0].toString(), point.geometry.getCoordinates()[1].toString());
             myMap.geoObjects.add(point);
             myMap.panTo(point.geometry.getCoordinates());
            }
@@ -28,7 +27,35 @@ function addPlacemark (address) {
 }
 
 function addRoute (address1, address2) {
-    var myRouter = ymaps.route([address1, address2], { mapStateAutoApply: true});
+    var startGeocoder = ymaps.geocode(
+        address1, {
+            results: 1
+        }
+    );
+    var finishGeocoder = ymaps.geocode(
+        address2, {
+            results: 1
+        }
+    );
+    startGeocoder.then(
+        function (res) {
+           if (res.geoObjects.getLength()) {
+            var point = res.geoObjects.get(0);
+            addToTable(address1, point.geometry.getCoordinates()[0].toString(), point.geometry.getCoordinates()[1].toString());
+           }
+        }
+    );
+    finishGeocoder.then(
+        function (res) {
+           if (res.geoObjects.getLength()) {
+            var point = res.geoObjects.get(0);
+            addToTable(address2, point.geometry.getCoordinates()[0].toString(), point.geometry.getCoordinates()[1].toString());
+           }
+        }
+    );
+    var myRouter = ymaps.route([address1, address2], { 
+        mapStateAutoApply: true
+    });
     myRouter.then(function(route) {
         myMap.geoObjects.add(route);
     });
@@ -44,11 +71,54 @@ function getCoords (address) {
         function (res) {
            if (res.geoObjects.getLength()) {
             var point = res.geoObjects.get(0);
-            $('#lat').val(point.geometry.getCoordinates()[0].toString());
-            $('#lon').val(point.geometry.getCoordinates()[1].toString());
+            addToTable(address, point.geometry.getCoordinates()[0].toString(), point.geometry.getCoordinates()[1].toString());
            }
         }
     );
+}
+
+function addCustomPlacemarks () {
+    start = new ymaps.GeoObject({
+        geometry: {
+            type: "Point",
+            coordinates: [50.034581, 36.229968]
+        },
+        properties: {
+            iconContent: 'Старт'
+        }
+    }, {
+        preset: 'islands#redStretchyIcon',
+        draggable: true
+    });
+    finish = new ymaps.GeoObject({
+        geometry: {
+            type: "Point",
+            coordinates: [50.034581, 36.220968]
+        },
+        properties: {
+            iconContent: 'Финиш'
+        }
+    }, {
+        preset: 'islands#greenStretchyIcon',
+        draggable: true
+    });
+    myMap.geoObjects.add(start);
+    myMap.geoObjects.add(finish);
+    $('#button4').addClass("hidden");
+    $('#button5').removeClass("hidden");
+}
+
+function addCustomRoute () {
+    var myRouter = ymaps.route([start.geometry.getCoordinates(), finish.geometry.getCoordinates()], { 
+        mapStateAutoApply: true
+    });
+    myRouter.then(function(route) {
+        myMap.geoObjects.add(route);
+    });
+}
+
+function addToTable (address, x, y) {
+    $('#table > tbody:last').append('<tr><th>' + address + '</th><th>' + x + '</th><th>' + y +'</th></tr>');
 }
 
 $("#button1").click(function() {
@@ -61,4 +131,12 @@ $("#button2").click(function() {
 
 $("#button3").click(function() {
     getCoords($('#address4').val());
+});
+
+$("#button4").click(function() {
+    addCustomPlacemarks();
+});
+
+$("#button5").click(function() {
+    addCustomRoute();
 });
--- src/main/webapp/map.jsp
+++ src/main/webapp/map.jsp
@@ -5,26 +5,35 @@
 <%@include file = "layout1.jsp"%>
 <br>
 <div class="form-block center-block" style="min-height: 900px;"> 
+    <div id="map" style="width: 600px; height: 400px"></div><br>
     <div>
         <p style="font-size: 20px;">Добавить метку:</p>
-        <input type="text" id="address1" style="width: 500px;" placeholder="Введите адрес">
-        <button id="button1">Готово</button>
+        <input type="text" id="address1" style="width: 478px; height: 29px;" placeholder="Введите адрес">
+        <button id="button1" class="btn btn-default btn-sm" style="padding: 0; margin: 0; height: 30px;">Готово</button>
     </div><br>
     <div>
         <p style="font-size: 20px;">Проложить маршрут:</p>
-        <input type="text" id="address2" style="width: 250px;" placeholder="Введите адрес">
-        <input type="text" id="address3" style="width: 250px;" placeholder="Введите адрес">
-        <button id="button2">Готово</button>
+        <input type="text" id="address2" style="width: 237px; height: 29px;" placeholder="Введите адрес">
+        <input type="text" id="address3" style="width: 237px; height: 29px;" placeholder="Введите адрес">
+        <button id="button2" class="btn btn-default btn-sm" style="padding: 0; margin: 0; height: 30px;">Готово</button>
     </div><br>
     <div>
         <p style="font-size: 20px;">Узнать координаты:</p>
-        <input type="text" id="address4" style="width: 500px;" placeholder="Введите адрес">
-        <button id="button3">Готово</button><br><br>
-        <p>Широта: <input id="lat"></p>
-        <p>Долгота: <input id="lon"></p>
-    </div>
-    <br><br>
-    <div id="map" style="width: 600px; height: 400px"></div>
+        <input type="text" id="address4" style="width: 478px; height: 29px;" placeholder="Введите адрес">
+        <button id="button3" class="btn btn-default btn-sm" style="padding: 0; margin: 0; height: 30px;">Готово</button>
+    </div><br>
+    <button id="button4" class="btn btn-default btn-block">Добавить подвижные метки</button>
+    <button id="button5" class="btn btn-default btn-block hidden">Проложить путь между подвижными метками</button><br>
+    <table class="table" id="table">
+    <thead>
+            <tr>
+                <th style="width: 50%;">Адрес</th>
+                <th style="width: 25%;">Широта</th>
+                <th style="width: 25%;">Долгота</th>
+            </tr>
+    </thead>
+    <tbody>
+    </table>
 </div>
 
 <script src="js/map.js"></script>
